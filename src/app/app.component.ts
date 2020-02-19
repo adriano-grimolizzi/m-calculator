86ad86eea79c5b945968b4506f20f9c17c89c55b
agrimolizzi <adriano.grimolizzi@amadeus.com>
2020-02-19 12:18:22 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'm-calculator';
+
+  public stack = 6;
+
+  public bigBlind = 1;
+
+  public ante = 0;
+
+  public numberOfPlayers = 9;
+
+  get smallBlind(): number { return this.bigBlind ? this.bigBlind / 2 : 0; }
+
+  get magriel(): number { return this.stack / (this.smallBlind + this.bigBlind + this.ante * this.numberOfPlayers); }
+
+  get color(): string {
+    if (this.magriel < 1) { return 'gray'; } else
+    if (this.magriel < 5) { return 'red'; } else
+    if (this.magriel < 10) { return 'orange'; } else { return 'green'; }
+  }
+  // get effectiveMagriel(): number { return this.magriel * (this.numberOfPlayers / 10); }
+
+  get magrielStyle() {
+    return { 'color': this.color };
+  };
 }
